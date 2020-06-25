37d56f6f329b905a3584af5899645e234284f4cd
mathan26 <mathan.se@gmail.com>
2020-06-25 17:38:51 +0530

    Commit : @Profile with default use cases
    Issue Faced:none
    Solution: none
    Learned:default the profile by adding default.

--- src/main/java/com/github/mathan26/springdependencyinjectionexamples/services/EnglishGreetingServiceImpl.java
+++ src/main/java/com/github/mathan26/springdependencyinjectionexamples/services/EnglishGreetingServiceImpl.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18nService")
 public class EnglishGreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - EN";
+        return "Hello - EN Profile Default Impl";
     }
 }
