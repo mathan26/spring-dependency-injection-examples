f5746bcb8b8160527f6d06bff2957a230e02550d
mathan26 <mathan.se@gmail.com>
2020-06-25 13:26:57 +0530

    Initial Commit

---
+++ src/main/java/com/github/mathan26/springdependencyinjectionexamples/SpringDependencyInjectionExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.github.mathan26.springdependencyinjectionexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDependencyInjectionExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDependencyInjectionExamplesApplication.class, args);
+	}
+
+}
